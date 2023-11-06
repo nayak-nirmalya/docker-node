f714f7745cd63b30e2ee1a98b0fe3a42119d1730
Nirmalya Nayak <n.nayak263@gmail.com>
2023-11-06 14:30:43 +0530

    added simple get route /

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hey, I am Node.js inside Docker Container.",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server Started on PORT: ${PORT}`));
